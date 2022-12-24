bf3ddf07cd02cc7c5dde2184b18f3ce271d50616
Dmitry <dmitry.mekhedov@mail.ru>
2022-12-24 23:41:02 +0300

    together

--- js/main.js
+++ js/main.js
@@ -4,6 +4,30 @@ $(document).ready(function() {
         $('.menu__burger,.menu__wrapper').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    
-   
+    //Слайдер-online
+    $('.online__slider').slick({
+        dots:true,
+        arrows:false,
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                   slidesToShow: 1,
+                   dots:true,
+                }
+            },
+        ]
+    });
+    //Слайдер-together
+    $('.slider-together').slick({
+        dots:false,
+        arrows:false,
+        slidesToShow: 1,
+        infinite: true,
+        variableWidth: true,
+        rows:2,
+        autoplay:true,
+        autoplaySpeed:500,
+    });
 });
