7e5f613b1ee4716cff2887d87fc8f1097d018021
Dmitry <dmitry.mekhedov@mail.ru>
2022-12-26 00:30:12 +0300

    ask

--- js/main.js
+++ js/main.js
@@ -4,6 +4,35 @@ $(document).ready(function() {
         $('.menu__burger,.menu__wrapper').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    //Акардеон
+    $('.item-ask__icon_1').click(function() {
+        $('.item-ask__icon_1,.item-ask__text_1').toggleClass('open');
+        
+    });
+    $('.item-ask__icon_2').click(function() {
+        $('.item-ask__icon_2,.item-ask__text_2').toggleClass('open');
+        
+    });
+    $('.item-ask__icon_3').click(function() {
+        $('.item-ask__icon_3,.item-ask__text_3').toggleClass('open');
+        
+    });
+    $('.item-ask__icon_4').click(function() {
+        $('.item-ask__icon_4,.item-ask__text_4').toggleClass('open');
+        
+    });
+    $('.item-ask__icon_5').click(function() {
+        $('.item-ask__icon_5,.item-ask__text_5').toggleClass('open');
+        
+    });
+    $('.item-ask__icon_6').click(function() {
+        $('.item-ask__icon_6,.item-ask__text_6').toggleClass('open');
+        
+    });
+
+
+
+
     //Слайдер-online
     $('.online__slider').slick({
         dots:true,
